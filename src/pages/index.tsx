1fe7059789eea93ecfe6ebec7c7b95bdb45a0a88
RafaelGoulartB <rafagoulartb@gmail.com>
2021-02-23 11:33:24 -0300

    📦 feat: add componets to index (#2)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
+import Head from "next/head";
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
+import CompletedChallenges from "../components/CompletedChallenges";
+import Countdown from "../components/Countdown";
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Home | Move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
