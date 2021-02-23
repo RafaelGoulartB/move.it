4e2644033cbea447c3161a252d622f0f81cba229
RafaelGoulartB <rafagoulartb@gmail.com>
2021-02-23 11:32:52 -0300

    📦 feat: add compleatedChallenges component (#2)

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,12 @@
+import styles from "../styles/components/CompletedChallenges.module.css";
+
+function CompletedChallenges() {
+  return (
+    <div className={styles.completedChallengesContainer}>
+      <span>Desafios completos:</span>
+      <span>5</span>
+    </div>
+  );
+}
+
+export default CompletedChallenges;
