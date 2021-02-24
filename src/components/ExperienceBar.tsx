8e3061a3f91e1d0d6129cb163356b4e64b8da9be
RafaelGoulartB <rafagoulartb@gmail.com>
2021-02-24 11:40:36 -0300

    📦 feat: add context to expirence bar (#3)

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,16 +1,29 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/ExperiencesBar.module.css'
 
 const ExperienceBar: React.FC = () => {
+  const { currentExperience, experienceToNextLevel } = useContext(
+    ChallengesContext
+  )
+
+  const porcentToNextLevel = Math.round(
+    (currentExperience * 100) / experienceToNextLevel
+  )
+
   return (
     <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
-        <div style={{ width: '50%' }} />
-        <span className={styles.currentExperience} style={{ left: '50%' }}>
-          300xp
+        <div style={{ width: `${porcentToNextLevel}%` }} />
+        <span
+          className={styles.currentExperience}
+          style={{ left: `${porcentToNextLevel}%` }}
+        >
+          {currentExperience}xp
         </span>
       </div>
-      <span>600 xp</span>
+      <span>{experienceToNextLevel} xp</span>
     </header>
   )
 }
